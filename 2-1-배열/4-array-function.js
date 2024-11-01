99fbfcdc2b8e5530b439e4bb81ce517032da77cf
soongu <naong526@naver.com>
2024-11-01 10:53:07 +0900

    js-2-1-배열 메서드 1

---
+++ 2-1-배열/4-array-function.js
@@ -0,0 +1,37 @@
+
+let foodList = ['닭꼬치', '볶음밥', '족발', '파스타'];
+
+// indexOf() : 배열의 특정 요소가 몇 번 인덱스에 있는지 알려줌
+let idx = foodList.indexOf('볶음밥');
+console.log(`index: ${idx}`);
+
+//음식중에 족발을 찾아서 보쌈으로 수정
+foodList[foodList.indexOf('족발')] = '보쌈';
+
+console.log(foodList);
+
+
+// includes() : 배열에 특정 요소가 있는지 유무 확인
+let flag = foodList.includes('파스타');
+console.log(`flag: ${flag}`);
+
+if (!foodList.includes('파스타')) {
+  // 음식 목록에 파스타가 없으면 
+}
+
+console.log('=========================');
+
+// slice() : 배열을 원하는 만큼 분할해서 복사배열로 리턴
+let numbers = [0, 1, 2, 3, 4, 5, 6];
+
+let slicedNumbers = numbers.slice(1, 3);
+console.log(`slicedNumbers: `, slicedNumbers);
+console.log(`numbers: `, numbers);
+
+let copy1 = numbers.slice(3); // end 생략시 끝까지 분할
+console.log(copy1);
+let copy2 = numbers.slice(); // 둘다 생략시 처음부터 끝까지 -> 전체복제
+console.log(copy2);
+
+
+
