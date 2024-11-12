4a3e9e864387de5fb6aea39747f4dea2d7e7b106
soongu <naong526@naver.com>
2024-11-12 14:41:23 +0900

    js-계산기 완성

--- CALCULATOR/js/app.js
+++ CALCULATOR/js/app.js
@@ -5,6 +5,7 @@ const MUL = 'x';
 const DIV = '/';
 //=========== 전역변수 영역 ==========//
 let currentResult = 0; // 연산이 누적될 변수
+const logArray = []; // 로그들을 저장할 변수
 
 //=========== DOM 가져오기 ===========//
 // 버튼들 가져오기
@@ -21,6 +22,9 @@ const $currentResult = document.getElementById('current-result');
 // 현재 계산 로그 영역 가져오기
 const $curCalc = document.getElementById('current-calculation');
 
+// 로그 목록 태그 가져오기
+const $logEntries = document.querySelector('.log-entries');
+
 //=========== 함수 정의 ===========//
 // 사칙연산 분기 처리 함수
 function operateNumber({ type, first, second }) {
@@ -36,7 +40,7 @@ function operateNumber({ type, first, second }) {
       result = first * second;
       break;
     case DIV:
-      result = first / second;
+      result = Math.floor(first / second);
       break;
   }
   return result;
@@ -65,6 +69,50 @@ function calculate(type) {
 
   // 5. 계산 로그를 화면에 렌더링
   $curCalc.textContent = descriptionLog;
+
+  // 6. 로그들을 배열에 쌓기
+  const logMessage = `${descriptionLog} = ${currentResult}`;
+  logArray.push(logMessage);
+
+  // 7. 로그를 로그목록에 최신로그 4개만 렌더링
+  const SHOW_LOG_NUMBER = 4;
+  const startIndex
+    = (logArray.length < SHOW_LOG_NUMBER)
+      ? 0
+      : logArray.length - SHOW_LOG_NUMBER;
+  
+  // logEntries 비우기
+  $logEntries.innerHTML = '';
+
+  for (let i = startIndex; i < logArray.length; i++) {
+    const $newLi = document.createElement('li');
+    $newLi.textContent = logArray[i];
+    $newLi.classList.add('log-entries__item');
+  
+    $logEntries.prepend($newLi);
+  }
+
+  // 8. 로그 개수가 4개가 되면 전체보기 버튼 생성
+  const $showAllBox = document.querySelector('.showAllBox');
+  if (
+    !$showAllBox.children.length &&
+    logArray.length >= SHOW_LOG_NUMBER
+  ) {
+    const $showAllBtn = document.createElement('button');
+    $showAllBtn.textContent = '전체 로그 보기';
+    $showAllBox.append($showAllBtn);
+
+    $showAllBtn.addEventListener('click', e => { 
+      $logEntries.innerHTML = '';
+      logArray.forEach(log => {
+        const $newLi = document.createElement('li');
+        $newLi.textContent = log;
+        $newLi.classList.add('log-entries__item');
+
+        $logEntries.prepend($newLi);
+      });
+    });
+  }
 
 }
 
